4225c0d07a645b538cb2294b463241a96da32da4
Negar <nyaghoobi@gmail.com>
2024-04-21 19:53:27 +0330

    in

---
+++ chapter4/lesson53_in.py
@@ -0,0 +1,14 @@
+print('a' in 'sajad')
+print('x' in 'sajad')
+
+print(2 in [11,2,3,4])
+
+print(33 in (11,2,3,[4]))
+
+if 'azx' in [11,22,['azx'],'a']:
+    print('yes')
+
+
+my_dict = {'k1':'v1','k2':'v2'}
+if 'k1' in my_dict.keys():
+    print('yesssssss')
