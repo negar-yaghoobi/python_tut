d6b1bf4d8248d89ec3e3481390094ce7d5888faf
Negar <nyaghoobi@gmail.com>
2024-04-21 19:52:51 +0330

    condition

---
+++ chapter4/lesson52_Condition.py
@@ -0,0 +1,40 @@
+if 2 > 11:
+    print('false')
+
+if 2>1 :
+    print('true')
+
+print('-----------------------')
+
+if 11 > 10:
+    print('yes')
+else:
+    print('no')
+
+if 11 < 10:
+    print('yes')
+else:
+    print('no')
+
+print('-----------------------')
+
+if 11 > 10:
+    print('yes')
+elif 112 >10:
+    print('yesss')
+else:
+    print('no')
+
+if 11 < 10:
+    print('yes')
+elif 112 >10:
+    print('yesss')
+else:
+    print('no')
+
+if 11 < 10:
+    print('yes')
+elif 112 < 10:
+    print('yesss')
+else:
+    print('no')
