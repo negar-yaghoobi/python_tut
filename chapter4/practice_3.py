26eb83831c92356932c9aa2e787f4a15626db62a
Negar <nyaghoobi@gmail.com>
2024-04-24 11:06:54 +0330

    update code

--- chapter4/practice_3.py
+++ chapter4/practice_3.py
@@ -3,7 +3,7 @@ price = int(input('enter the price:'))
 if price >= 0:
     if price > 1000:
         print('too expensive')
-    elif price > 500 and price <= 1000:
+    elif 500 < price <= 1000:    #price > 500 and price <= 1000
         print('expensive')
     elif price > 100 and price <= 500:
         print('normal')
