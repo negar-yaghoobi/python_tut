6057fb73b0388770b949dde1a7b62cd10e5b4596
Negar <nyaghoobi@gmail.com>
2024-04-24 07:50:17 +0330

    add file practice_5 : middle of list

---
+++ chapter4/practice_5.py
@@ -0,0 +1,8 @@
+my_list = [11, 23, 45, 677, 83, 90, 110, 514]
+length = len(my_list)
+print(length)
+middle = length//2
+if length % 2 != 0 :
+    print(my_list[middle])
+else:
+    print((my_list[middle-1] + my_list[middle])/2)
