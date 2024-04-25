9247bb321dfc8f692c3ea4e3b3b078cf28f61c74
Negar <nyaghoobi@gmail.com>
2024-04-25 18:42:58 +0330

    add codes about tuple unpacking

--- chapter5/lesson75_for ,dictionary.py
+++ chapter5/lesson75_for ,dictionary.py
@@ -7,16 +7,27 @@ studends = {
 for s in studends:
     print(s)
 
+print('------------------------------------')
+
 for s in studends.items():
     print(s)
+
+print('------------------------------------')
 
 for s in studends.values():
     print(s)
 
+print('------------------------------------')
 
 for s in studends.keys():
     print(s)
 
 print('------------------------------------')
+
 for a,b in studends.items():
+    print(a)
+
+print('------------------------------------')
+
+for (a,b) in studends.items():
     print(a)
