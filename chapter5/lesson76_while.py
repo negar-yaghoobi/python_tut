450ae9f256f0a04278881ae8e5ea747e6c28aae2
Negar <nyaghoobi@gmail.com>
2024-04-25 19:58:41 +0330

    add file lesson76 : while

---
+++ chapter5/lesson76_while.py
@@ -0,0 +1,21 @@
+i = 0
+while i <5:
+    print(i)
+    i +=1
+
+print('------------------')
+
+j = 20
+while 11 < j:
+    print(j)
+    j -= 1
+else:
+    print('else')
+
+print('------------------')
+
+while False:
+    print('false')
+else:
+    print('true')
+
