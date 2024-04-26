b43fb62abceff8e29be034b06125d3ecc3be1b97
Negar <nyaghoobi@gmail.com>
2024-04-26 09:57:15 +0330

    add space

--- chapter5/lesson78_continue.py
+++ chapter5/lesson78_continue.py
@@ -1,6 +1,5 @@
 i = 0 
 while i < 10 :
-    #print(i)
     if i == 5:
         i +=1
         continue
@@ -11,7 +10,6 @@ print('end')
 
 print('---------------------------')
 
-j = 0
 for j in [1,3,4,5,6,80]:
     if j == 5:
         continue
