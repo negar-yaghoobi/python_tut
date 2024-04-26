13005d2c1167bbeff7d26765a8a5edf3f21cb23f
Negar <nyaghoobi@gmail.com>
2024-04-26 08:18:28 +0330

    break

---
+++ chapter5/lesson77_break.py
@@ -0,0 +1,16 @@
+i = 0 
+for i in [1,2,3,4,5,6]:
+    if i == 4:
+        break
+    print(i)
+print('end')
+
+print('-------------------')
+
+j = 0
+while j < 10:
+    if j == 5:
+        break
+    print(j)
+    j +=1
+print('end')
