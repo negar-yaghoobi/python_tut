c29cd52fd097f09bd148c01bf768e10930d15cc7
Negar <nyaghoobi@gmail.com>
2024-05-14 20:16:59 +0330

    add file practice12_2

---
+++ chapter4/practice_12-2.py
@@ -0,0 +1,12 @@
+user_name = input('enter your username : ')
+password = input('enter your password : ')
+if user_name == password:
+    print('the two values should not be equal.')
+if len(password) < 8:
+    print('the length of the password must be at least 8 characters.')
+if len(password) > 20 :
+    print('the length of the password must be at most 20 characters.')
+if len(user_name) < 4 :
+    print('the length of the username must be at least 4 characters.')
+else:
+    print('the entries are correct and username and password has been saved.')
