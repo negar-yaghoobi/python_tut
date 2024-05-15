c361aed591ce2b367b556ee335588f0585e38574
Negar <nyaghoobi@gmail.com>
2024-05-15 12:48:45 +0330

    write practice17

---
+++ chapter4/practice_17.py
@@ -0,0 +1,6 @@
+name = input('enter your name :').lower()
+names_list = ['sajad','negar','amir','zahra','ali','mary']
+if name in names_list :
+    print('please login')
+else:
+    print('You are not allowed to enter')
