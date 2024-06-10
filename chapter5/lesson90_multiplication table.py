02f341979d6023a220ae478ac0ebe51859b360c1
Negar <nyaghoobi@gmail.com>
2024-06-10 21:05:35 +0330

    add solutions about multication table

---
+++ chapter5/lesson90_multiplication table.py
@@ -0,0 +1,34 @@
+# for i in range(1,11):
+#     for j in range(1,11):
+#         print(f'{i} * {j} = {i*j}')
+
+# =======================================================
+
+for i in range(1,11):
+    print()
+    for j in range(1,11):
+        print(i*j, end=' ')
+
+# =======================================================
+
+for i in range(1,11):
+    print()
+    for j in range(1,11):
+        print(f'{i*j:3}', end=' ')
+
+# =======================================================
+
+
+
+# print(list(range(1,10,1)))
+# print(list(range(2,20,2)))
+# print(list(range(3,30,3)))
+# print(list(range(4,40,4)))
+# print(list(range(5,50,5)))
+# print(list(range(6,60,6)))
+# print(list(range(7,70,7)))
+# print(list(range(8,80,8)))
+# print(list(range(9,90,9)))
+# print(list(range(10,100,10)))
+
+# a,b,c:
