4a4e9bfbf5176b0850ab03902eee5d680ac957cb
Negar <nyaghoobi@gmail.com>
2024-06-11 17:00:30 +0330

    add codes about encryption

---
+++ chapter5/practice_3.py
@@ -0,0 +1,17 @@
+text = list(input('enter string: '))
+print(text)
+
+list_save =[]
+for char in text:
+    list_save.append(str(ord(char) + 12))
+
+print(','.join(list_save))
+
+
+
+
+
+
+
+
+#=========================================
