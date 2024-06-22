6d0058253eac8e8d3a75a3aee2445754421a2253
Negar <nyaghoobi@gmail.com>
2024-06-22 20:18:10 +0330

    add codes about arguments of functions

---
+++ chapter6/lesson104_input argument.py
@@ -0,0 +1,17 @@
+def print_welcome_message(name = 'sajad'):
+    print('hi')
+    print(f'welcome {name}.')
+
+print_welcome_message('negar')
+
+print_welcome_message()
+
+
+#------------------------------------------------
+
+def print_2_welcome_message(name,family):
+    print('hi')
+    print(f'your name is {name} and your family is {family} ')
+
+
+print_2_welcome_message('negar','sarmast')
