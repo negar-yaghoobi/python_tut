911cafb62dadf397976f79adb2e2581edf0e1087
Negar <nyaghoobi@gmail.com>
2024-06-23 13:24:29 +0330

    add codes about return in function

---
+++ chapter6/lesson105,106_return in function.py
@@ -0,0 +1,35 @@
+def sum (num1, num2):
+    sum_nums = num1 + num2
+    return sum_nums
+    
+result = sum(2, 3)
+print(result)
+
+r = sum(3,4) * 3
+print(r)
+
+print('------------------------------------------------')
+
+def sum (num1, num2):
+    print(num1 + num2)
+
+sum(2, 3)
+
+print('------------------------------------------------')
+
+def sum (num1, num2):
+    sum_nums = num1 + num2
+    return sum_nums
+    
+
+print(sum(2, 3))
+
+print('------------------------------------------------')
+
+def sum (num1, num2):
+    print('in the function')
+    sum_nums = num1 + num2
+    return sum_nums
+    
+
+print(sum(2, 3))
