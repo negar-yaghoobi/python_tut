b76fb07915731d7811e9f426af119f12c753103f
Negar <nyaghoobi@gmail.com>
2024-06-23 12:23:02 +0330

    practice about loop in math

---
+++ chapter6/practice_1.py
@@ -0,0 +1,18 @@
+import random
+
+choice = random.choice(range(1,10))
+print(f'choice : {choice}')
+
+sum = 0
+count = 0
+while count != 3 :
+    sum = sum +choice
+    if choice in [1,2,4]:
+        count +=1
+    if choice %2 != 0 :
+        choice = choice * 3 + 1
+    else:
+        choice = choice / 2
+
+
+print(f'sum : {sum}')
