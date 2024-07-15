563bb7807ed9b835fc9d3a2ed06bf4886cc5a0ea
Negar <nyaghoobi@gmail.com>
2024-07-15 19:59:13 +0330

    add examples about bulit in function and del

---
+++ chapter6/lesson119.py
@@ -0,0 +1,34 @@
+print('hi')       # built in function
+
+#============================================ type of functions
+
+def my_func():
+    print('hi')
+
+
+print(type(my_func))
+
+
+print(type(len))
+
+
+#============================================  del
+
+len = 110
+print(len)
+
+del len
+
+print(len([1,2]))
+
+
+#============================================  del
+
+a = 110
+
+print(a)
+
+del a
+
+print(a)      # error
+
