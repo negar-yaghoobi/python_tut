ef90831a25070983b1b4cbdfc10701c5ce4fffdd
Negar <nyaghoobi@gmail.com>
2024-07-17 15:33:23 +0330

    help() and docstring

---
+++ chapter6/lesson121.py
@@ -0,0 +1,21 @@
+#===============================
+
+help(len)
+
+print(30 * '--')
+#==============================
+def sum_numbers(num1, num2):
+    ''' this function use for sum of two numbers.
+    num1 : the first number
+    num2 : the second number '''
+
+    return num1 + num2
+
+
+print(f'sum = {sum_numbers(2, 3)}')
+
+
+print(30 * '--')
+#==============================
+
+help(sum_numbers)
