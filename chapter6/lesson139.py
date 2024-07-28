67a4a6f24b726c912fce6e5d0d6093b82ae9f486
Negar <nyaghoobi@gmail.com>
2024-07-28 11:51:20 +0330

    add to this program a list that change in function

--- chapter6/lesson139.py
+++ chapter6/lesson139.py
@@ -1,6 +1,15 @@
-def max_min_sum_len(*args):
-    list_number = list(args)
-    return f'min:{min(list_number)}, max:{max(list_number)}, sum:{sum(list_number)}, length:{len(list_number)}'
+def max_min_sum_len(*args,list_of_nums):
+    if len(args) == 0 :
+        print('error : at least enter one number')
+        
+    list_of_nums = list(args)
+    for i in range(args):
+        list_of_num.append(i)
+    return f'min:{min(list_of_nums)}, max:{max(list_of_nums)}, sum:{sum(list_of_nums)}, length:{len(list_of_nums)}'
     
-print(max_min_sum_len(10, 11, 12, 13, 14, 15, 32, 0, 1, 2, 90))
+
+list_of_num = []
+print(max_min_sum_len(10, 11, 12, 13, 14, 15, 32, 0, 1, 2, 90, list_of_nums=list_of_num))
+
+print(list_of_num)
     
