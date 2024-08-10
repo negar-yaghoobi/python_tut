60ac4d827a23f7a94c95cbd95f0329c22174edf5
Negar <nyaghoobi@gmail.com>
2024-08-10 16:38:50 +0330

    add class syntax

---
+++ chapter8/lesson171-175.py
@@ -0,0 +1,12 @@
+# object oriented programming (oop)
+
+
+# class syntax
+
+class NameOfClass:
+    def __init__(self, a1, a2):
+        self.first = a1
+        self.second = a2
+    
+    def custom_method(self):
+        pass
