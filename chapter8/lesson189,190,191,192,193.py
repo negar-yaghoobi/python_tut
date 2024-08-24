272fb1a7ae9b6e3703ab8cd68e4dcb3dd257ef00
Negar <nyaghoobi@gmail.com>
2024-08-24 16:15:28 +0330

    rename file from chapter8/lesson189,190,191.py to chapter8/lesson189,190,191,192,193.py

--- chapter8/lesson189,190,191,192,193.py
+++ chapter8/lesson189,190,191,192,193.py
@@ -10,7 +10,7 @@ class Time:
         self.check_seconds()
 
     def check_hour(self):
-        if isinstance(self.hour, int):
+        if isinstance(self.hour, int):          # check integer nubmer
             if self.hour > 24:
                 self.hour = 23
             elif self.hour < 0:
@@ -19,7 +19,7 @@ class Time:
             self.hour = 0
 
     def check_minutes(self):
-        if isinstance(self.minutes, int):
+        if isinstance(self.minutes, int):          # check integer nubmer
             if self.minutes > 60:
                 self.minutes = 59
             elif self.minutes < 0:
@@ -28,7 +28,7 @@ class Time:
             self.minutes = 0
 
     def check_seconds(self):
-        if isinstance(self.seconds, int):
+        if isinstance(self.seconds, int):        # check integer nubmer
             if self.seconds > 60:
                 self.seconds = 59
             elif self.seconds < 0:
@@ -125,11 +125,56 @@ class Time:
     def __str__(self):
         return f'{self.hour:02}:{self.minutes:02}:{self.seconds:02}'
 
+    def __add__(self, other):                              # use for Adding the two objects        ~ # operator overloading with __add__
+        if isinstance(other, Time):                        # check that two objects are in same class 
+
+            seconds = self.seconds + other.seconds
+            minutes = self.minutes + other.minutes + (seconds // 60)
+            hour = self.hour + other.hour + (minutes // 60)
+            new_time = Time(hour % 24, minutes % 60, seconds % 60)        # create new object of class time that return  (*)
+            return new_time
+        
+        return NotImplemented
+    
+    # operator loading ==
+    def __eq__(self, other):              
+        return (self.hour == other.hour) \
+            and (self.minutes == other.minutes) \
+            and (self.seconds == other.seconds)
+    
+    # operator loading >
+    def __gt__(self, other):              
+        return (self.hour > other.hour) \
+                or (self.hour == other.hour and self.minutes > other.minutes) \
+                or (self.hour == other.hour and self.minutes == other.minutes and self.seconds > other.seconds)
+            
+
+# =================================================== part 1 
 
 clock = Time(10, 6, 0 )
-
 print(clock.hour)
 print(clock.minutes)
 print(clock.seconds)
 
 print(clock)
+
+
+#========================================================= part 2      # operator overloading
+
+obj1 = Time(22, 55, 49)
+obj2 = Time(22, 55, 49)
+
+# Adding the two objects
+result = obj1 + obj2
+print(result)                     # result is new object of class time that create in return of __add__ method (*)
+
+# part 3 
+
+print(obj1 == obj2)               # use __eq__ in class for this operator
+
+print(obj1 > obj2)                # use __gt__ in class for this operator
+
+
+
+#============================================= dir ()
+print(dir(obj1))
