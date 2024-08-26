c885f7c1f14ace3f842c98cf9718f8b30cf44233
Negar <nyaghoobi@gmail.com>
2024-08-26 14:39:41 +0330

    add examples about importr and from ..import

---
+++ chapter9/lesson198_3.py
@@ -0,0 +1,14 @@
+from lesson198_2 import to_power_2, function4
+from lesson198_1 import Car
+from lesson198_2 import *         # it is better not to use
+
+
+list_1 = [1,2,3,4,5]
+
+print(list(map(to_power_2, list_1)))
+
+
+obj2 = Car(20)
+print(obj2)
+
+function4()
