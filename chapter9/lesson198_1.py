e5db69ebc4dc5d17464a17ec40126705becb5ef3
Negar <nyaghoobi@gmail.com>
2024-08-26 08:09:19 +0330

    add 4 function to file lesson 198-1.py

---
+++ chapter9/lesson198_1.py
@@ -0,0 +1,14 @@
+def function1():
+    print('this is function 1.')
+
+def function2():
+    print('this is function 2.')
+
+def function3():
+    print('this is function 3.')
+
+def function4():
+    print('this is function 4.')
+
+function1()
+function3()
