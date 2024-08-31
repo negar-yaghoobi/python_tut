c1dea14c629fe5a347b665caf5955d210fa7048c
Negar <nyaghoobi@gmail.com>
2024-08-31 18:58:53 +0330

    add comment for pip commands

---
+++ chapter10/lesson207.py
@@ -0,0 +1,10 @@
+# pip install name's package                             install package
+# pip uninstall name's package                           uninstall package
+# pip install -u                                         install package and update it 
+# pip list                                               list of packages and version that use in project
+# pip list --outdated                                    list of packages and version that use in project and needs update
+# pip list -o                                            list of packages and version that use in project and needs update
+# pip freeze                                             list of packages and version that add to project
+# pip freeze > requirements.txt                          add list of packages and version that add to project to requirements.txt file
+# type  requirements.txt                                 return list of packages in requirements.txt file
+# pip install -r requirements.txt                        install all packages in requirements.txt file
