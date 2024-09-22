260262b224da255d90647659d7dbdafdd8ea14cb
Negar <nyaghoobi@gmail.com>
2024-09-22 14:30:07 +0330

    add one example about try and except

--- chapter12/lesson218,219.py
+++ chapter12/lesson218,219.py
@@ -10,3 +10,32 @@ except:
     print('exception occured')
 
 print('end...')
+
+# ================================================
+print(30 * '--')
+
+
+nums = [100, -10, 30, 46, 11]
+
+print(f'list of numbers: {nums}')
+print('select one of them with it\'s index.')
+
+
+index = int(input('enter index: '))
+
+
+def some_error():
+    print(2 / 0)
+
+
+def get_number_of_list_with_index(list_of_items, index):
+    return list_of_items[index]
+
+
+try:
+    some_error()
+    print(get_number_of_list_with_index(nums, index))
+except:
+    print('invalid index...')
+
+print('some other code runnig...')
