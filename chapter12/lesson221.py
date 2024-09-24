c8661eace1e758e13aa865d08b42921c09956600
Negar <nyaghoobi@gmail.com>
2024-09-24 14:16:20 +0330

    add examples about create error by using raise and handle it by using try_except

--- chapter12/lesson221.py
+++ chapter12/lesson221.py
@@ -9,7 +9,21 @@ user_income = int(input('Enter your number : '))
 print(calc_income(user_income))
 
 
-#=========================================================== raise 
+#=========================================================== create error by using raise 
+
+def calc_income(income):
+    if income < 0 :
+        raise Exception('income should not be a negetive number!')            # create error
+    
+    print('first')
+    return income * 2
+
+user_income = int(input('Enter your number : '))
+print(calc_income(user_income))
+
+print('second')
+
+#======================================================= handling error that create by using raise
 
 def calc_income(income):
     if income < 0 :
