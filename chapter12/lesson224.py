3ec84d372159cd29e3d3d4c3c1fddb3cca00ae4d
Negar <nyaghoobi@gmail.com>
2024-09-25 12:26:57 +0330

    finally in try_except

--- chapter12/lesson224.py
+++ chapter12/lesson224.py
@@ -14,3 +14,21 @@ else:       # if there is no error
     print('ELSE')
 
 print('END...')
+
+# ================================================ finally in try_axcept
+
+a = 1
+b = int(input('enter a number: '))
+
+try:   # try to run this code
+    print(a/b)
+except ZeroDivisionError:    # if this exception happended
+    print('EXCEPTION')
+except ModuleNotFoundError:       #  if this exception happended
+    print('another exception')
+else:       # if there is no error
+    print('ELSE')
+finally:
+    print('FINALLY')
+
+print('END...')
