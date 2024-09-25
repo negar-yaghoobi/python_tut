5efeffdb1d35ddcfb6ff49ac7cb9b469fde8a200
Negar <nyaghoobi@gmail.com>
2024-09-25 12:16:36 +0330

    else in try_except

---
+++ chapter12/lesson224.py
@@ -0,0 +1,16 @@
+#============================================ else in try-except
+a = 1
+b = int(input('enter a number: '))
+
+try:   # try to run this code
+    print(a/b)
+except ZeroDivisionError:    # if this exception happended
+    print('EXCEPTION')
+except ModuleNotFoundError:       #  if this exception happended
+    print('another exception')
+except Exception:
+    print('some other error')
+else:       # if there is no error
+    print('ELSE')
+
+print('END...')
