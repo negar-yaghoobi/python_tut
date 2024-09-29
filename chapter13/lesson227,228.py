7cbbd71bc0f22e1c6cd023864128249d842d8315
Negar <nyaghoobi@gmail.com>
2024-09-29 12:31:38 +0330

    open filein python and write in it

---
+++ chapter13/lesson227,228.py
@@ -0,0 +1,9 @@
+# name = input('enter your file name : ')
+
+# file = open(f'{name}.txt', 'w')           # w : write
+file = open('names_1.txt', 'w')               # open or create file in mode 'w'
+
+file.write('hello sajad...\n')               # write in file
+file.write('hello sajadiiiii...')     
+file.write('hello negar...')  
+file.write('\nhello negariiii...')
