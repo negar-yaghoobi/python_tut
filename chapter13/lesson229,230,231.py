21f1329d82dd49106218e61f54e4b0d6d0cf2010
Negar <nyaghoobi@gmail.com>
2024-09-29 13:44:21 +0330

    add readlines() and readline() methods and 2 ways for move line by line on the information of a file.txt in python file

--- chapter13/lesson229,230,231.py
+++ chapter13/lesson229,230,231.py
@@ -1,10 +1,34 @@
+#============================================= read()
+
 file = open('names_2.txt', 'r')
 
 a = file.read()
-print(type(a))
-print(a)
+# print(type(a))
+# print(a)
 
-print(a.split('\n'))
+# print(a.split('\n'))
 
-for line in a.split('\n'):
-    print(line + 'y')
+# for line in a.split('\n'):
+#     print(line + 'y')
+for name in a.split('\n'):                    # The first way is to move line by line on the information of a file
+    print(f'hello {name.title()}')
+
+#============================================ readlines()
+file = open('names_3.txt', 'r')
+
+b = file.readlines()
+
+print(b)
+
+#=============================================== readline()
+
+# The second way is to move line by line on the information of a file
+
+file = open('names_3.txt', 'r')
+
+while True:
+    line = file.readline()
+    print(line,end='')
+    if line == '':
+        break
+print('end....')
