9d15e543daed5d7f1a597350e7e86b01bc28421c
Negar <nyaghoobi@gmail.com>
2024-10-08 13:43:56 +0330

    relative path in files

---
+++ chapter13/lesson240.py
@@ -0,0 +1,20 @@
+# when run python code in chapter13 folder and file and python code are in the same folder
+
+open('file_1.txt', 'w')
+
+#==================================
+# when run python code out chapter13 folder (project folder) and file and python code are in the same folder
+
+open('chapter13/file_1.txt', 'w')
+
+#======================================
+
+# when run python code in project folder and file and python code are not in the same folder
+
+open('file_1.txt', 'w')
+
+#==================================
+# when run python code in chapter13 folder and file and python code are not in the same folder
+
+open('../file_1.txt', 'w')
+
