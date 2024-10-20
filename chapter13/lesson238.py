e8d50c75b2eae9e2427f5b2bb5dfd358ba3c7121
Negar <nyaghoobi@gmail.com>
2024-10-20 09:31:36 +0330

    add another solution for this practice

--- chapter13/lesson238.py
+++ chapter13/lesson238.py
@@ -35,3 +35,44 @@ for str in list_info:
 avr = sum_marks / len(list_info)
 
 print(f'average of marks = {avr}')
+
+
+#================================================= another solution
+
+print('--- Current grades: ---')
+
+with open('grades.txt', 'r') as grades_file:
+    all_data = grades_file.read()
+    print(all_data)
+
+new_grades = []
+
+yes_no_new_grades = input('Do you want add new grades?')
+
+if yes_no_new_grades == 'yes':
+    while True:
+        name = input('Enter name: ')
+
+        if name == 'exit':
+            break
+
+        grade = input('Enter grade: ')
+        new_grades.append({
+            'name': name,
+            'grade': int(grade)
+        })
+
+with open('grades.txt', 'a') as grades_file:
+    for student_grade in new_grades:
+        grades_file.write(f"\n{student_grade['name']} {student_grade['grade']}")
+
+all_student_grades = []
+with open('grades.txt', 'r') as reader:
+    all_data = reader.read()
+    for line in all_data.split('\n'):
+        x = line.split(' ')
+        name = x[0]
+        grade = x[1]
+        all_student_grades.append(grade)
+    
+print(sum(all_student_grades)/len(all_student_grades))
